0d39e42f85567cc1339ff0c7584ef5f72ab87302
Rafael Fazzolino <fazzolino29@gmail.com>
2017-05-22 20:32:09 -0300

    Adicionando Slam PC

---
+++ src/mcl/ConfigDetails.java
@@ -0,0 +1,22 @@
+package mcl;
+
+import java.awt.Point;
+
+import javax.swing.JButton;
+
+import lejos.robotics.mapping.NavigationPanel;
+
+public class ConfigDetails extends NavigationPanel {
+	protected static final long serialVersionUID = 1L;
+
+	protected static final int FRAME_WIDTH = 1000;
+	protected static final int FRAME_HEIGHT = 800;
+	protected static final int NUM_PARTICLES = 200;
+	protected static final String TITLE = "MCL Test";
+	protected static final int INITIAL_ZOOM = 150;
+	protected static final Point INITIAL_VIEW_START = new Point(-150,-30);
+
+	protected static final JButton newRefButton = new JButton("Procurar novas Referências");
+	protected static final JButton getPose = new JButton("Verificar Posição");
+	protected static final String mapFileName = "map.svg";
+}
---
+++ src/mcl/Localization.java
@@ -0,0 +1,132 @@
+package mcl;
+
+import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.*;
+import javax.swing.*;
+import lejos.robotics.mapping.MenuAction;
+import lejos.robotics.mapping.NavigationModel.NavEvent;
+import lejos.robotics.localization.*;
+
+/**
+ * Localization - Class that process and control NXT Robot.
+ * @author Rafael Fazzolino
+ *
+ */
+public class Localization extends ConfigDetails {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static MCLPoseProvider mcl;
+  
+  	/**
+   	* Create a Frame with options to connect to the NXT.
+   	*/
+  	public static void main(String[] args) throws Exception {
+  		(new Localization()).run();
+  	}
+  	
+  	/*Constructor that create a Panel that contains all options
+  	 * to use MCL with NXT.*/
+  	public Localization() {
+  		buildGUI();
+  	}
+  
+  	/**
+  	 * Create the GUI
+  	 */
+  	@Override
+  	protected void buildGUI() {		
+	    super.buildGUI();
+	    
+	    /* Adding buttons get Pose and New References */
+		commandPanel.add(getPose);
+		commandPanel.add(newRefButton);
+		
+		/* Until connected, the buttons must be disable */
+		getPose.setEnabled(false);
+		newRefButton.setEnabled(false);
+	
+		/* When Get pose is pressed, we call the MCL Pose
+		* to read the environment and get the actual pose.
+		*/
+		getPose.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				/* Get the actual pose, reading the environment. */
+				model.getPose();
+				/* Get actual particles */
+				model.getRemoteParticles();
+				/* Estimated the actual pose */
+				model.getEstimatedPose();
+				//System.out.println("Max weight:" + model.getParticles().getMaxWeight());
+				/* Get readings of the particles */
+				model.getRemoteReadings();
+				
+				/* Disable the button */
+				getPose.setEnabled(false);
+			}
+		});
+		
+		/* When the New Reference button is pressed: */
+		newRefButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				/* Make a random move */
+				model.randomMove();
+				/* Get the new particles */
+				model.getRemoteParticles();
+			}
+		});
+  	}
+  
+  	/**
+  	 * When the mouse is clicked in the map
+  	 */
+  	@Override
+	protected void popupMenuItems(Point p, JPopupMenu menu) { 
+	    /* Get information of the particle closest to the local clicked */
+	    menu.add(new MenuAction(NavEvent.FIND_CLOSEST, "Informações da Partícula", p, model, this));
+	}
+	
+	/**
+	 * When an information is received from the robot
+	 */
+	@Override
+	public void eventReceived(NavEvent navEvent) {
+		/* If the estimated pose has been sent, we can enable the getPose button*/
+		if (navEvent == NavEvent.ESTIMATED_POSE) {
+			getPose.setEnabled(true);
+		}
+	}
+	
+	/**
+	 * When the connection is OK
+	 */
+	@Override
+	public void whenConnected() {
+		/* Enable debug mode */
+		model.setDebug(true);
+		/* Load the map and generate the first particles sending both to the robot */
+		model.loadMap(mapFileName);
+		zoomSlider.setValue(INITIAL_ZOOM);
+		mapPanel.viewStart = INITIAL_VIEW_START;
+		model.generateParticles();
+		
+		/* Enable buttons */
+		getPose.setEnabled(true);
+		newRefButton.setEnabled(true);
+	}
+	
+	/**
+	 * Execute the program
+	 */
+	public void run() throws Exception {
+		/* Instantiate the MCLPoseProvider */
+		mcl = new MCLPoseProvider(null,NUM_PARTICLES,0);
+		
+		/* Adding the MCLPoseProvider into the model */
+		model.setMCL(mcl);
+		
+		/* Open it in a JFrame window */
+	    openInJFrame(this, FRAME_WIDTH, FRAME_HEIGHT, TITLE, Color.white);
+	}
+}
